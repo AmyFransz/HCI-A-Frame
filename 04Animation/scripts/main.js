07700afe539d8bd6a80798ba554b54006b9b8138
AmyFransz <43185341+AmyFransz@users.noreply.github.com>
2021-06-25 10:14:02 +0200

    java

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,24 @@
 
-
-// // changing individual properties with code and using setInterval
-var rotationSpeed = 0.01;
+// changing individual properties with code and using setInterval
+var rotationSpeed = -0.05;
+var goDown = true;
 var myOtherBox = document.getElementById('myOtherBox');
 
- function spin(){
-    myOtherBox.object3D.rotation.x += rotationSpeed;
+function spin(){
+	if(goDown){
+		rotationSpeed += -0.05;
+	}else{
+		rotationSpeed += 0.05;
+	}
+	if(rotationSpeed < -10){
+		goDown = false;
+	}
+	if(rotationSpeed > 10){
+		goDown = true;
+	}
+	myOtherBox.object3D.rotation.x += rotationSpeed;
+    myOtherBox.object3D.rotation.y += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
- }
+}
 
- setInterval(spin, 16); //equivalent to 60 fps
+setInterval(spin, 16); //equivalent to 60 fps
